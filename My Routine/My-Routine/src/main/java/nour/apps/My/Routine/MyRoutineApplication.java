2a75523f3c2fcdad612543d96662c4305e1f4a29
nrubeh <nrubeh@accumed.ae>
2025-11-19 11:41:44 +0300

    First Commit

---
+++ My Routine/My-Routine/src/main/java/nour/apps/My/Routine/MyRoutineApplication.java
@@ -0,0 +1,14 @@
+package nour.apps.My.Routine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRoutineApplication {
+
+	public static void main(String[] args) {
+        System.out.println("Hello World/My Routine");
+        SpringApplication.run(MyRoutineApplication.class, args);
+	}
+
+}
---
+++ My Routine/My-Routine/src/test/java/nour/apps/My/Routine/MyRoutineApplicationTests.java
@@ -0,0 +1,13 @@
+package nour.apps.My.Routine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyRoutineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
